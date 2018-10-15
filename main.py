b5bf3ba7f5675cde642fb00c2f0e6bc192251ee4
setite <setyte@gmail.com>
2018-10-15 13:18:20 -0500

    okay everything is fixed, just need to add verifications

--- main.py
+++ main.py
@@ -31,20 +31,20 @@ def index():
 @app.route('/blog', methods=['GET', 'POST'])
 def blog():
 
-    blog_id = Blog.query.get(blog.id)
-    # blog_id = request.args.get('id')
+    # blog_id = Blog.query.get(blog.id)
+    blog_id = request.args.get('id')
 
     if blog_id:
         blog = Blog.query.get(blog_id)
         # title = blog.title
         # body = blog.body
-        return render_template('blog_entry.html')
+        return render_template('blog_entry.html', blog=blog)
     else:
         blogs = Blog.query.all()
         # first of the pair matches to {{}} in for loop inblogs the .html template, second of the pair matches to variable declared above
         return render_template('blogs.html', blogs=blogs)
-
-@app.route('/newpost', methods=['POST'])
+        
+@app.route('/newpost', methods=['GET', 'POST'])
 def new_post():
     
     if request.method == 'POST':
@@ -55,9 +55,9 @@ def new_post():
         db.session.add(new_blog)
         db.session.commit()
        
-        # url = "/blog?id=" + str(new_blog.id)
-        # return redirect(url)
-        return render_template('blog_entry.html', title=new_blog.title, body=new_blog.body)
+        url = "/blog?id=" + str(new_blog.id)
+        return redirect(url)
+        # return render_template('blog_entry.html', title=new_blog.title, body=new_blog.body)
 
     else:
         return render_template('newpost.html', title="Build a Blog!")
